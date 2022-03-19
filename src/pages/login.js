4203b2455a5c7b550a49fbd9486198977e355f90
rohit katbamna <rohitkatbamna@protonmail.com>
2022-03-19 11:56:14 +0530

    19TH MARCH 2022 11:56 AM COMMIT NO.1

---
+++ src/pages/login.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../scss/login.scss";
+function Login() {
+	return (
+		<div className="container-fluid login_all_of_it">
+			<div className="row login_row_one">
+				<div className="col-xxl-3 col-xl-3 col-lg-4 col-md-5 col-sm-5  mx-auto login_row_col_one">
+					<label htmlFor="Email" className="fw-bold mb-2 fs-3">
+						Email
+					</label>
+					<br />
+					<input
+						className="form-control form-control-lg"
+						type="text"
+						placeholder="email@example.com"
+						aria-label="email@example.com"
+					/>
+					<br />
+					<label htmlFor="password" className="fw-bold mb-2 fs-3">
+						Password
+					</label>
+					<br />
+					<input
+						className="form-control form-control-lg"
+						type="password"
+						placeholder="Password"
+						aria-label="password"
+					/>
+				</div>
+			</div>
+			<div className="row login_row_two">
+				<button
+					type="button"
+					className="btn btn-primary mx-auto col-xxl-3 col-xl-3 col-lg-4 col-md-5 col-sm-5 mt-4 fs-3 fw-bold">
+					Login
+				</button>
+				<p className="text-center fw-bold fs-4 mt-3">
+					Dont Have Account <Link to="/signup">Sign Up</Link>
+				</p>
+			</div>
+		</div>
+	);
+}
+export default Login;
---
+++ src/pages/signup.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../scss/signup.scss";
+function Signup() {
+	return (
+		<div className="container-fluid signup_all_of_it">
+			<div className="row signup_row_one">
+				<div className="col-xxl-3 col-xl-3 col-lg-4 col-md-5 col-sm-5  mx-auto signup_row_col_one">
+					<label htmlFor="Name" className="fw-bold mb-2 fs-3">
+						Name
+					</label>
+					<br />
+					<input
+						className="form-control form-control-lg"
+						type="text"
+						placeholder="John Doe"
+						aria-label="John Doe"
+					/>
+					<br />
+					<label htmlFor="Email" className="fw-bold mb-2 fs-3">
+						Email
+					</label>
+					<br />
+					<input
+						className="form-control form-control-lg"
+						type="text"
+						placeholder="email@example.com"
+						aria-label="email@example.com"
+					/>
+					<br />
+					<label htmlFor="password" className="fw-bold mb-2 fs-3">
+						Password
+					</label>
+					<br />
+					<input
+						className="form-control form-control-lg"
+						type="password"
+						placeholder="Password"
+						aria-label="password"
+					/>
+				</div>
+			</div>
+			<div className="row login_row_two">
+				<button
+					type="button"
+					className="btn btn-primary mx-auto col-xxl-3 col-xl-3 col-lg-4 col-md-5 col-sm-5 mt-4 fs-3 fw-bold">
+					Sign Up
+				</button>
+				<p className="text-center fw-bold fs-4 mt-3">
+					Already Have Account <Link to="/">Sign In</Link>
+				</p>
+			</div>
+		</div>
+	);
+}
+export default Signup;
