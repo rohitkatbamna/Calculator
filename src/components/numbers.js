fe6c0e8ddcbf84f5bc2b083bdc10d9ce14687363
rohit katbamna <rohitkatbamna@protonmail.com>
2022-03-19 15:14:37 +0530

    19TH MARCH 2022 03:14 PM COMMIT NO.5

--- src/components/numbers.js
+++ src/components/numbers.js
@@ -2,10 +2,7 @@ import React, { useState } from "react";
 import "../scss/numbers.scss";
 
 function Numbers({ number, setNumber }) {
-	let numarr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-	const [acc, setAcc] = useState("");
-	setNumber(acc);
-	console.log(number);
+	let numarr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 	return (
 		<>
 			<div className="container-fluid numbers_all_of_it">
@@ -13,9 +10,9 @@ function Numbers({ number, setNumber }) {
 					{numarr.map((num) => {
 						return (
 							<button
-								className="btn col-5 btn-primary btn-lg mx-2 my-2"
+								className="btn col-xxl-5 col-xl-5 col-lg-5 col-md-4 col-sm-6 btn-primary btn-lg mx-2 my-2"
 								value={num}
-								onClick={() => setAcc((prev) => prev + num)}>
+								onClick={() => setNumber((prev) => prev + num)}>
 								{num}
 							</button>
 						);
--- src/pages/calculator.js
+++ src/pages/calculator.js
@@ -2,26 +2,44 @@ import React, { useState } from "react";
 import Numbers from "../components/numbers";
 import Operators from "../components/operators";
 import "../scss/cal.scss";
+import equal from "../assets/equal.png";
 function Calculatorpage() {
-	const [number, setNumber] = useState(0);
-	const [op, setOp] = useState("");
-	const [acc, setAcc] = useState("");
-	let vl = number + op;
+	const [number, setNumber] = useState("");
+	function onEqual() {
+		const newArr = number.split(" ");
+		let one = Number(newArr[0]);
+		let two = Number(newArr[2]);
+		if (newArr[1] === "*") setNumber(one * two);
+		if (newArr[1] === "+") setNumber(one + two);
+		if (newArr[1] === "/") setNumber(one / two);
+		if (newArr[1] === "-") setNumber(one - two);
+	}
 	return (
 		<div className="container-fluid cal_all_of_it">
-			<div className="row card cal_card col-xxl-4 col-xl-4 col-lg-5 col-md-6 col-sm-12 shadow">
+			<div className="row card cal_card col-xxl-4 col-xl-4 col-lg-5 col-md-7 col-sm-12 shadow">
 				<div className="card-body cal_card_body">
-					<input type="text" value={vl} />
+					<input type="text" value={number} />
 					<div className="cal_card_body_add_grid_here">
-						<Operators op={op} setOp={setOp} setNumber={setNumber} />
+						<Operators number={number} setNumber={setNumber} />
 						<Numbers number={number} setNumber={setNumber} />
 					</div>
-					<button
-						className="btn btn-primary  btn-lg "
-						value="="
-						onClick={() => setOp("=")}>
-						=
-					</button>
+					<div className="cal_card_body_one_more_grid">
+						<button className="btn btn-primary" value="=" onClick={onEqual}>
+							<img src={equal} alt="=" height={50} width={50} />
+						</button>
+						<button
+							className="btn btn-primary fs-2"
+							value="C"
+							onClick={() => setNumber("")}>
+							C
+						</button>
+						<button
+							className="btn btn-primary fs-2"
+							value="."
+							onClick={() => setNumber(".")}>
+							.
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
