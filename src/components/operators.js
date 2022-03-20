f29c539864f047bb2d3ea6b74b77309618185e4e
rohit katbamna <rohitkatbamna@protonmail.com>
2022-03-20 09:44:08 +0530

    20TH MARCH 2022 09:43 AM COMMIT NO.1

--- src/components/operators.js
+++ src/components/operators.js
@@ -6,28 +6,27 @@ import divide from "../assets/divide.png";
 
 import "../scss/operator.scss";
 function Operators({ number, setNumber }) {
+	function ops(prev, op) {
+		let arr = prev.split("");
+		if (
+			arr[arr.length - 1] === "/" ||
+			arr[arr.length - 1] === "+" ||
+			arr[arr.length - 1] === "-" ||
+			arr[arr.length - 1] === "*"
+		) {
+			arr[arr.length - 1] = op;
+			let neww = arr.join("");
+			return neww;
+		}
+		return prev + op;
+	}
 	return (
 		<div className="container-fluid operator_all_of_it">
 			<div className="row operator_row">
 				<div className="col-3 operator_row_col_one">
 					<button
 						className="btn btn-primary"
-						onClick={() =>
-							setNumber((prev) => {
-								let arr = prev.split("");
-								if (
-									arr[arr.length - 1] === "/" ||
-									arr[arr.length - 1] === "+" ||
-									arr[arr.length - 1] === "-" ||
-									arr[arr.length - 1] === "*"
-								) {
-									arr[arr.length - 1] = "+";
-									let neww = arr.join("");
-									return neww;
-								}
-								return prev + "+";
-							})
-						}
+						onClick={() => setNumber((prev) => ops(prev, "+"))}
 						value="plus">
 						<img src={plus} alt="+" height={50} width={50} />
 					</button>
@@ -35,20 +34,7 @@ function Operators({ number, setNumber }) {
 						className="btn btn-primary mt-3"
 						value="-"
 						onClick={() =>
-							setNumber((prev) => {
-								let arr = prev.split("");
-								if (
-									arr[arr.length - 1] === "/" ||
-									arr[arr.length - 1] === "+" ||
-									arr[arr.length - 1] === "-" ||
-									arr[arr.length - 1] === "*"
-								) {
-									arr[arr.length - 1] = "-";
-									let neww = arr.join("");
-									return neww;
-								}
-								return prev + "-";
-							})
+							setNumber((prev) => setNumber((prev) => ops(prev, "-")))
 						}>
 						<img src={minus} alt="-" height={50} width={50} />
 					</button>
