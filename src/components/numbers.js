2efba9afd9d444a746b33f8c88eeeabb1a107329
rohit katbamna <rohitkatbamna@protonmail.com>
2022-03-20 10:11:49 +0530

    20TH MARCH 2022 10:11 AM COMMIT NO.3 ZERO PROBLEM FIXED

--- src/components/numbers.js
+++ src/components/numbers.js
@@ -12,7 +12,16 @@ function Numbers({ number, setNumber }) {
 							<button
 								className="btn col-xxl-5 col-xl-5 col-lg-5 col-md-4 col-sm-6 btn-primary btn-lg mx-2 my-2"
 								value={num}
-								onClick={() => setNumber((prev) => prev + num)}>
+								onClick={() =>
+									setNumber((prev) => {
+										let newv = prev + num;
+										if (newv[0] === "0") {
+											let result = newv.replace(0, "");
+											return result;
+										}
+										return prev + num;
+									})
+								}>
 								{num}
 							</button>
 						);
